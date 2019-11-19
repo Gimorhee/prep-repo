827496e64729ad0efabffc22a6fe08512b5a3e5a
Danny Rhee <dongyunrhee@gmail.com>
2019-11-18 22:54:53 -0800

    Simple Typescript tutorials

--- November2019/Typescript/greeter.js
+++ November2019/Typescript/greeter.js
@@ -12,3 +12,19 @@ function greeter(person) {
 }
 var user = { firstName: "Danny", lastName: "Rhee" };
 document.body.textContent = greeter(user);
+// ------------------------------------------------------------------- //
+var notSure;
+notSure = 3;
+var isDone = false;
+var list = [1, 2, 3];
+var x = ["1", 2];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Green"] = 2] = "Green";
+    Color[Color["Yellow"] = 3] = "Yellow";
+})(Color || (Color = {}));
+var c = Color.Red;
+var colorName = Color[2];
+create({ pop: 0 });
+create(null);
--- November2019/Typescript/greeter.ts
+++ November2019/Typescript/greeter.ts
@@ -18,3 +18,21 @@ function greeter(person: Person) {
 let user = { firstName: "Danny", lastName: "Rhee"};
 
 document.body.textContent = greeter(user);
+
+// ------------------------------------------------------------------- //
+
+let notSure : any; notSure = 3;
+let isDone : boolean = false;
+let list : number[] = [ 1, 2, 3];
+let x : [string, number] = ["1", 2];
+enum Color { Red = 1, Green, Yellow }
+let c : Color = Color.Red;
+let colorName : string = Color[2];
+
+declare function create(o: object | null) : void;
+create({ pop: 0 });
+create(null);
+
+let someValue : any = "This is String";
+let strLength1 : number = (<string>someValue).length;
+let strLength2 : number = (someValue as string).length;
